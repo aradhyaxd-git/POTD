668a9d5e9c343bab7bd8a52643f923bf84504dbd
Aradhya <2k23.cs2312107@gmail.com>
2025-02-26 22:44:59 +0530

    KADANE ALGO

---
+++ February/26Feb25/1749_subarrays.java
@@ -0,0 +1,42 @@
+/*1749. Maximum Absolute Sum of Any Subarray
+You are given an integer array nums. The absolute sum of a subarray 
+[numsl, numsl+1, ..., numsr-1, numsr] is abs(numsl + numsl+1 + ... + numsr-1 + numsr).
+
+Return the maximum absolute sum of any (possibly empty) subarray of nums.
+
+Note that abs(x) is defined as follows:
+
+If x is a negative integer, then abs(x) = -x.
+If x is a non-negative integer, then abs(x) = x.
+ 
+
+Example 1:
+
+Input: nums = [1,-3,2,3,-4]
+Output: 5
+Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
+
+Example 2:
+
+Input: nums = [2,-5,1,-4,3,-2]
+Output: 8
+Explanation: The subarray [-5,1,-4] has absolute sum = abs(-5+1-4) = abs(-8) = 8. */
+
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+      int n= nums.length;
+      int maxiMax=Integer.MIN_VALUE; int sum=0;
+      for(int i=0;i<n;i++){
+        sum+=nums[i];
+        maxiMax=Math.max(maxiMax,sum);
+        if(sum<0) sum=0;
+      }
+      int miniMax=Integer.MAX_VALUE;sum=0;
+      for(int i=0;i<n;i++){
+        sum+=nums[i];
+        miniMax= Math.min(miniMax,sum);
+        if(sum>0) sum=0;  
+      }
+      return Math.max(Math.abs(miniMax),maxiMax);    
+    }
+}
